86772ce5948279e1929846ab835cfe8735562f2d
unknown <glorenzo2001@gmail.com>
2023-01-13 17:01:03 +0000

    Refactoring

--- nba-scores/app.js
+++ nba-scores/app.js
@@ -84,23 +84,38 @@ const warriorsGames = [{
     }
 ]
 
-const ulParent = document.createElement('ul');
-for (let game of warriorsGames) {
-    const {homeTeam, awayTeam} = game;
+const makeChart = (games, targetTeam) => {
+    const ulParent = document.createElement('ul');
+    for (let game of games) {
+        const gameLi = document.createElement('li');
+        gameLi.innerHTML = getScoreLine(game)
+       
+        gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss');
+        ulParent.append(gameLi);
+    }
+    return ulParent;
+};
+
+const isWinner = ({homeTeam, awayTeam}, targetTeam) => {
+    const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+    return target.isWinner
+};
+
+const getScoreLine = ({homeTeam, awayTeam}) => {
     const {team: hTeam, points: hPoints} = homeTeam;
     const {team: aTeam, points: aPoints} = awayTeam;
-    const gameLi = document.createElement('li');
     const teamLine = `${aTeam} @ ${hTeam}`
     let scoreLine;
     if (aPoints > hPoints) {
-       scoreLine = `<b>${aPoints}</b>-${hPoints}`
+        scoreLine = `<b>${aPoints}</b>-${hPoints}`;
     } else {
-        scoreLine = `${aPoints}-<b>${hPoints}</b>`
+        scoreLine = `${aPoints}-<b>${hPoints}</b>`;
     }
-    const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
-    gameLi.classList.add(warriors.isWinner ? 'win' : 'loss')
-    
-    gameLi.innerHTML = `${teamLine} ${scoreLine}`
-    ulParent.append(gameLi)
-}
-document.body.prepend(ulParent)
+    return `${teamLine} ${scoreLine}`;
+};
+
+const chart1 = makeChart(warriorsGames, 'Golden State');
+document.body.prepend(chart1);
+
+const chart2 = makeChart(warriorsGames, 'Houston');
+document.body.prepend(chart2);
