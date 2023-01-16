8e376f48eee0dc744f874cfdfe263433b684ea71
unknown <glorenzo2001@gmail.com>
2023-01-16 11:29:58 +0000

    Updated background color to h1 after click

--- change-color/app.js
+++ change-color/app.js
@@ -1,5 +1,10 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'indigo', 'violet'];
 
+const changeColor = function() {
+    const h1 = document.querySelector('h1');
+    h1.style.color = this.style.backgroundColor;
+}
+
 const container = document.querySelector('#boxes');
 
 for (let color of colors) {
@@ -7,4 +12,5 @@ for (let color of colors) {
     box.style.backgroundColor = color;
     box.classList.add('box');
     container.appendChild(box)
+    box.addEventListener('click', changeColor);
 }
