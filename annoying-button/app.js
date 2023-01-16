4fb0f006c9111a32c20d658637d7e368dfbdd521
unknown <glorenzo2001@gmail.com>
2023-01-16 10:51:03 +0000

    Added event click button

--- annoying-button/app.js
+++ annoying-button/app.js
@@ -6,4 +6,9 @@ btn.addEventListener('mouseover', function() {
     const width = Math.floor(Math.random() * window.innerWidth)
     btn.style.left = `${width}px`;
     btn.style.top = `${height}px`;
+});
+
+btn.addEventListener('click', function() {
+    btn.innerText = 'You got me :(';
+    document.body.style.backgroundColor = 'green';
 })
